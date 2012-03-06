48391cb3e07789f08135473238218058149a6828
Tair Assimov <tair@assimov.net>
2012-03-06 15:44:12 +0200

    Refactor Reservation tests to use Shoulda matchers

--- test/unit/reservation_test.rb
+++ test/unit/reservation_test.rb
@@ -6,42 +6,31 @@ class ReservationTest < ActiveSupport::TestCase
     @new_reservation = Factory.build(:reservation)
   end
   
-  test "should not save reservation without a book_id" do
-    @new_reservation.book_id = nil
-    assert @new_reservation.invalid?
-    assert @new_reservation.errors[:book_id].any?
-    assert !@new_reservation.save
-  end
+  should validate_presence_of(:book_id)
+  should validate_presence_of(:email)
+  should allow_value("free").for(:state)
+  should allow_value("reserved").for(:state)
+  should_not allow_value("invalid@e").for(:email)
   
-  test "should not save reservation with invalid state" do
-    @new_reservation.save
-    @new_reservation.state = 'invalid'
-    assert !@new_reservation.save
-    assert @new_reservation.errors[:state].any?
-  end
-  
-  test "should not save with empty email" do
-    @new_reservation.email = nil
-    assert !@new_reservation.save
-    assert @new_reservation.errors[:email].any?
-  end
-
-  test "should not save with invalid email" do
-    @new_reservation.email = 'invalid@e'
-    assert !@new_reservation.save
-    assert @new_reservation.errors[:email].any?
-  end
-  
-  test "should not allow reservation if the book already reserved" do
+  should "not allow reservation if the book already reserved" do
     reservation = Factory(:reservation, state: 'reserved')
     copy = Reservation.new(reservation.attributes)
     assert !copy.save
     assert_match /book has been already reserved/, copy.errors[:book_id].join
   end
   
-  test "should be initially reserved" do
-    assert @new_reservation.save
-    assert_equal 'reserved', @new_reservation.state
+  context 'reservation record' do
+    setup do
+      @new_reservation.save
+    end
+    
+    subject { @new_reservation }
+    
+    should_not allow_value("invalid").for(:state)
+  
+    should "be initially reserved" do
+      assert_equal 'reserved', @new_reservation.state
+    end
   end
 
 end
