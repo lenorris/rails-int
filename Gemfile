c4a192510e93daf539f7aba3ccf5463ee7bfb54e
Tero Parviainen <tero.parviainen@eficode.fi>
2012-03-07 18:28:21 +0200

    Added User model with secure passwords

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem 'rails', '3.1.1'
 # Bundle edge Rails instead:
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
 
+gem 'bcrypt-ruby', '~> 3.0.0'
+
 # Heroku needed gems
 gem 'heroku'
 gem 'execjs'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120213190148) do
+ActiveRecord::Schema.define(:version => 20120307162011) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -31,5 +31,12 @@ ActiveRecord::Schema.define(:version => 20120213190148) do
   end
 
   add_index "reservations", ["book_id"], :name => "index_reservations_on_book_id"
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
